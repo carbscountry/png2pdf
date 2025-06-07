fd6a370d0bbef52e4e0b52e68207e89118d281e4
carbscountry <s1270161forbisness@gmail.com>
2025-06-08 01:01:19 +0900

    edit README main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import os
 from PIL import Image
 
+_dir = 'map_of_accounting'
 def main():
     # Irresistbleディレクトリのパス
-    dir_path = os.path.join(os.path.dirname(__file__), 'Irresistble')
+    dir_path = os.path.join(os.path.dirname(__file__), _dir)
     # p*.pngファイルを取得し、数字部分でソート
     files = [f for f in os.listdir(dir_path) if f.lower().endswith('.png') and f.lower().startswith('p')]
     files.sort(key=lambda x: int(x[1:4]))  # pXXX.png のXXX部分でソート
     images = [Image.open(os.path.join(dir_path, f)).convert('RGB') for f in files]
     if images:
-        images[0].save(os.path.join(os.path.dirname(__file__), 'Irresistble.pdf'), save_all=True, append_images=images[1:])
-        print('Irresistble.pdfを作成しました')
+        images[0].save(os.path.join(os.path.dirname(__file__), f'{_dir}.pdf'), save_all=True, append_images=images[1:])
+        print('pdfを作成しました')
     else:
         print('画像が見つかりません')
 
